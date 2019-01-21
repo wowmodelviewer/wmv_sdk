5b6a48d6210de52ace06a1ee73fa7adc97999cf7
Jeromnimo <jerome@bausse.fr>
2019-01-21 23:43:11 +0100

    WMV-364 : update sdk bat init file to use VS2017

--- wmv_set_env.bat
+++ wmv_set_env.bat
@@ -3,7 +3,7 @@ echo Setting up environment for WoW Model Viewer development...
 set WMV_SDK_BASEDIR=%CD%
 echo Base SDK directory set to : %WMV_SDK_BASEDIR%
 set PATH=%WMV_SDK_BASEDIR%\CMake\bin;%WMV_SDK_BASEDIR%;%PATH%
-for /f "usebackq tokens=*" %%i in (`vswhere.exe -legacy -version "[14.0,15.0)" -property InstallationPath`) do (
+for /f "usebackq tokens=*" %%i in (`vswhere.exe -legacy -version "[15.0,16.0)" -property InstallationPath`) do (
 set VSDIR=%%i
 )
 call "%VSDIR%\Common7\Tools\VsDevCmd.bat"
