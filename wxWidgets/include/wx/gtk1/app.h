5b719685f72f6a839ae73658f6a64c5ac20a5cb1
Jeromnimo <jerome@bausse.fr>
2021-01-31 15:22:24 +0100

    Update wxWidgets to 3.0.5. Compilation used : nmake.exe -f makefile.vc BUILD=debug SHARED=0 UNICODE=1 USE_OPENGL=1

--- wxWidgets/include/wx/gtk1/app.h
+++ wxWidgets/include/wx/gtk1/app.h
@@ -2,7 +2,6 @@
 // Name:        wx/gtk1/app.h
 // Purpose:
 // Author:      Robert Roebling
-// Id:          $Id: app.h 37065 2006-01-23 02:28:01Z MR $
 // Copyright:   (c) 1998 Robert Roebling, Julian Smart
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
@@ -14,12 +13,14 @@
 #include "wx/icon.h"
 #include "wx/strconv.h"
 
+typedef struct _GdkVisual GdkVisual;
+
 //-----------------------------------------------------------------------------
 // classes
 //-----------------------------------------------------------------------------
 
-class WXDLLIMPEXP_CORE wxApp;
-class WXDLLIMPEXP_BASE wxLog;
+class WXDLLIMPEXP_FWD_CORE wxApp;
+class WXDLLIMPEXP_FWD_BASE wxLog;
 
 //-----------------------------------------------------------------------------
 // wxApp
@@ -31,14 +32,13 @@ public:
     wxApp();
     virtual ~wxApp();
 
-    /* override for altering the way wxGTK intializes the GUI
+    /* override for altering the way wxGTK initializes the GUI
      * (palette/visual/colorcube). under wxMSW, OnInitGui() does nothing by
      * default. when overriding this method, the code in it is likely to be
      * platform dependent, otherwise use OnInit(). */
     virtual bool OnInitGui();
 
     // override base class (pure) virtuals
-    virtual bool Yield(bool onlyIfNeeded = FALSE);
     virtual void WakeUpIdle();
 
     virtual bool Initialize(int& argc, wxChar **argv);
@@ -46,18 +46,20 @@ public:
 
     static bool InitialzeVisual();
 
-#ifdef __WXDEBUG__
-    virtual void OnAssert(const wxChar *file, int line, const wxChar *cond, const wxChar *msg);
+    virtual void OnAssertFailure(const wxChar *file,
+                                 int line,
+                                 const wxChar *func,
+                                 const wxChar *cond,
+                                 const wxChar *msg);
 
     bool IsInAssert() const { return m_isInAssert; }
-#endif // __WXDEBUG__
 
-    gint            m_idleTag;
+    int             m_idleTag;
     void RemoveIdleTag();
 
     unsigned char  *m_colorCube;
 
-    // Used by the the wxGLApp and wxGLCanvas class for GL-based X visual
+    // Used by the wxGLApp and wxGLCanvas class for GL-based X visual
     // selection.
     void           *m_glVisualInfo; // this is actually an XVisualInfo*
     void           *m_glFBCInfo; // this is actually an GLXFBConfig*
@@ -67,12 +69,9 @@ public:
 
 private:
     // true if we're inside an assert modal dialog
-#ifdef __WXDEBUG__
     bool m_isInAssert;
-#endif // __WXDEBUG__
 
     DECLARE_DYNAMIC_CLASS(wxApp)
-    DECLARE_EVENT_TABLE()
 };
 
 #endif // __GTKAPPH__
